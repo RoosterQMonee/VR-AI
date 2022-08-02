3d3fbbaa2aa960b645a39c637eb12b326ae2c3ae
RoosterQMonee <82356323+RoosterQMonee@users.noreply.github.com>
2022-08-02 06:09:37 -0400

    Delete engine directory

--- engine/utils/light.py
+++
@@ -1,6 +0,0 @@
-from utils.vector import Normalize
-
-class Light: # simple directional light
-    def __init__(self, position):
-        self.position = position
-        self.direction = Normalize(self.position)
