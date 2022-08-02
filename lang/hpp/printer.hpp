8e508f4312b1098535dbfc45f8a2a6f8803b742f
RoosterQMonee <82356323+RoosterQMonee@users.noreply.github.com>
2022-08-02 06:15:20 -0400

    Delete lang directory

--- lang/hpp/printer.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <string>
-
-#include "types.hpp"
-
-std::string pr_str(Value *value, bool print_readably = false);
--- lang/printer.cpp
+++
@@ -1,5 +0,0 @@
-#include "hpp/printer.hpp"
-
-std::string pr_str(Value *value, bool print_readably) {
-    return value->inspect(print_readably);
-}
